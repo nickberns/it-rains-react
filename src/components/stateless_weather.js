74f72ea4c272192d9a65d943a712765cdadbb575
nickberns <nick@bluelightweb.co.nz>
2018-09-16 10:24:08 +1200

    fix spacing

--- src/components/stateless_weather.js
+++ src/components/stateless_weather.js
@@ -4,8 +4,8 @@ const SWeather = (props) => {
     return(
         <div>
         {props.country && props.city && <p>Location: {props.city}, {props.country}</p>}
-        {props.temperature && <p>Temperature: {props.temperature}</p>}
-        {props.humidity && <p>Humidity: {props.humidity}</p>}
+        {props.temperature && <p>Temperature: {props.temperature} *C</p>}
+        {props.humidity && <p>Humidity: {props.humidity} %</p>}
         {props.description && <p>Conditions:  {props.description}</p>}
         {props.error && <p>{props.error}</p>}
 
